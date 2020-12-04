3eba6653bf0be3b4f78f92e62d36dc6e95a03954
LiMaPe <65661847+LiMaPe@users.noreply.github.com>
2020-12-04 13:56:55 +0100

    project setup

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
-//Require and configure dotenv.
+//JavaScript Object to act as the app API endpoint.
+let projectData = {};
+
+//Require and config dotenv. Config assigns the content of .env file to process.env.
 require("dotenv").config();
 
 //Define the port and API key variables, process.env has the keys and values as defined in the .env file.
 const port = process.env.PORT
 const apiKey = process.env.APIKEY
-console.log(port);
-console.log(apiKey);
+
+//Require express and set up an instance of app
+const express = require('express')
+const app = express()
+
+//Middleware
+//Sets the body-parse to parsing the URL-encoded data with the querystring library.
+app.use(bodyParser.urlencoded({ extended: false }));
+//Parse application/json
+app.use(bodyParser.json());
+
+//Cors for cross origin allowance 
+const cors = require('cors');
+
+//Enables all CORS requests
+app.use(cors());
+
+//Connect the server-side and client-side (browser) code.
+app.use(express.static("website"));
+
+//Setup express server
+app.listen(port, () =>
+  console.log(`weather-app listening on port ${port}`),
+);
